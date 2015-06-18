477062cdc9a1e92fd1f83bd9ad035f52589b6238
Lau Siaw Young <lausiawyoung@gmail.com>
2015-06-18 17:42:37 +0800

    Add email mailing list

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def homepage
   end
+
+  def add_to_mailing_list
+    puts "hi"
+    debugger
+    render text: "ok", status: 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   get 'welcome/homepage'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +8,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#homepage'
+
+  post 'welcome/add_to_mailing_list' => 'welcome#add_to_mailing_list'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
